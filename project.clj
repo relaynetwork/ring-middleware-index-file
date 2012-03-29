0e751c0389a4e8d193f32c877c1cc92ab62b15c1
Paul and Kyle <kburton@relaynetwork.com>
2012-03-29 11:46:53 -0400

    new version for development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/ring-middleware-index-file "1.0.0"
+(defproject com.relaynetwork/ring-middleware-index-file "1.0.1-SNAPSHOT"
   :description "Ring Middleware for serving up an index file."
   :dependencies [
     [org.clojure/clojure "1.2.1"]
