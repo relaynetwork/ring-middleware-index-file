7474e9effe5c9d0478ef8e76bd80269c515c815a
Paul and Kyle <kburton@relaynetwork.com>
2012-04-26 10:52:05 -0400

    add deploy strategy

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject com.relaynetwork/ring-middleware-index-file "1.0.4-SNAPSHOT"
   :description "Ring Middleware for serving up an index file."
+  :lein-release {:deploy-via :clojars}
   :dependencies [
     [org.clojure/clojure "1.2.1"]
     [ring "0.2.5"]])
